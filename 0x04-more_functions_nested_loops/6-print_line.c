21473cfe50098522d015c318123f9b8ac6037bfc
Humphrey <odhiambohumphrey-78@gmail.com>
2023-05-25 19:41:10 +0300

    still line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,20 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
+		_putchar(92);
+		_putchar('n');
 	}
 	else if (n > 0)
 	{
-	for (; n > 0; n--)
+	for (; n >= 1; n--)
 	{
-		_putchar('_');
+		_putchar(95);
 	}
+		_putchar(92);
+		_putchar('n');
 	}
 	else
 	{
 	}
-	_putchar(36);
 	_putchar('\n');
 }
