cd5bdc1b8b18988df3044884c8345076a1fc2b75
Humphrey <odhiambohumphrey-78@gmail.com>
2023-05-30 23:26:33 +0300

    not well screwed

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * puts_half - prints second half of a string
+ * @str: string
+ * Return: (Success)
+ */
+void puts_half(char *str)
+{
+	int n, b;
+
+	for (b = 0; str[b] != '\0';)
+	{
+		str++;
+		b++;
+	}
+	if (b % 3 != 0)
+	{
+		n = (b - 1) / 2;
+		n = n - 2;
+
+		for (; n <= b; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else if (b % 2  == 0)
+	{
+		n = b / 2;
+		n = n - 2;
+
+	for (; n <= b; n++)
+	{
+		_putchar(str[n]);
+	}
+	}
+	_putchar('\n');
+}
