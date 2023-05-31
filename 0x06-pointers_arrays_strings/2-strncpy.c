192e89d090e20d0702c5fd71950fd38ea9b87b6c
Humphrey <odhiambohumphrey78@gmail.com>
2023-05-31 16:32:05 +0000

    just push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - copies string
+ * @dest: where it is to be copied
+ * @src: string to be copied
+ * @n: number of characters to be copied
+ * Return: (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+
+	char *p;
+
+	p = dest;
+
+	for (b = 0; b <= n; b+
+		
