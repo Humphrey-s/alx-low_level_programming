4628558f3812a107ee7b147ca302d9f62f8d6d8c
Humphrey <odhiambohumphrey-78@gmail.com>
2023-06-10 21:22:39 +0300

    argc first simple

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments itself
+ * Return: (Success)
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+return (0);
+}
