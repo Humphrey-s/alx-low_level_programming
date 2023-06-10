d3b28804a5141aa7e83d31792cbeca4a619a09bc
Humphrey <odhiambohumphrey-78@gmail.com>
2023-06-10 21:39:41 +0300

    argc now

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments itself
+ * Return: (Success)
+ */
+int main(int argc, char __attribute__ ((unused)) **argv)
+{
+	printf("%d\n", argc - 1);
+return (0);
+}
