87bf244c9fbe769169313e1576e04e9fca0228b1
Humphrey <odhiambohumphrey-78@gmail.com>
2023-06-12 21:20:37 +0300

    hahahaha memory

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 
-	ptr = malloc(sizeof(int) * size);
+	ptr = malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
@@ -25,7 +25,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-	ptr = malloc(sizeof(unsigned int) * size);
+	ptr = malloc(sizeof(char) * size);
 	memset(ptr, c, size);
 
 	return (ptr);
