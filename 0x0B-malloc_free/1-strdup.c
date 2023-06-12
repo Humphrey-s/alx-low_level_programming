dbe6dfb313046e8a8331a6d2a6df7a09ca4243b6
Humphrey <odhiambohumphrey-78@gmail.com>
2023-06-12 22:07:30 +0300

    hahahaha + we killed the strncpy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _strdup - puts a copy of a string in a newly allocated memory
+ * @str: string
+ * Return: (Success)
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int b;
+
+	b = strlen(str);
+	p = malloc(sizeof(char) * b);
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	else
+	{
+	strncpy(p, str, b);
+	return (p);
+	}
+}
