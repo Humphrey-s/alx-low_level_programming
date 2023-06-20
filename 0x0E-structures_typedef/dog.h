18d009656f805cdac86aa7278a5b4bdca34083a1
Humphrey <odhiambohumphrey-78@gmail.com>
2023-06-20 08:34:26 +0300

    new name for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  *
  * contains and defines new varibles for the description of a dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
