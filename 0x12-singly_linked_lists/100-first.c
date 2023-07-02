15b2bbf9db9a0bdd8d59b9e7c08fa8e28843218b
Humphrey <odhiambohumphre78@gmail.com>
2023-07-02 22:44:48 +0300

    print befoe main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_first - write or print before printf
+ * Return: (Success)
+ */
+void print_first(void) __attribute__ ((constructor));
+void print_first(void)
+{
+char *str = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+printf("%s", str);
+}
