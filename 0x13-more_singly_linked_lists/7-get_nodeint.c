eba9a68f7e15f55fc26b9081617065b5e24c1ec1
Humphrey <odhiambohumphre78@gmail.com>
2023-07-04 20:00:24 +0300

    nth none of list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -22,7 +22,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		i++;
 	}
-
-	return (head);
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (head);
+	}
 	}
 }
