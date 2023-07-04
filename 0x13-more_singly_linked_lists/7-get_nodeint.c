d7c7163caa510c88314404f5a27d85aeae6dfaab
Humphrey <odhiambohumphre78@gmail.com>
2023-07-04 19:56:47 +0300

    nth none of list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - returns the nth node
+ * @head: pointer to first node of lists
+ * @index: the nth position of node
+ * Return: (Success)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (head);
+	}
+}
