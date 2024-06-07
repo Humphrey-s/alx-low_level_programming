49b0133ec1e5751f711c055bdd27cf11ebd19577
greybeard <greybeard@gmail.com>
2024-06-07 09:08:53 +0300

    jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+#include "search_algos.h"
+/**
+ * jump_search - jump search algorithm
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: return first index where value is allocated
+ *         else -1 if value is not present
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0, jump_step = sqrt(size);
+	size_t j;
+
+	if (!array)
+		return (-1);
+
+	for (j = jump_step; j < size || i < size; j = j + jump_step)
+	{
+		if (array[j] >= value)
+			break;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i = j;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i, j);
+
+	while (i <= j && i < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+
+		i++;
+	}
+
+return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 #include <stddef.h>
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void searching_in(int *array, size_t from, size_t size);
